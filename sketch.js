2f3cec480beb0b3f4a00e246af61404b64063e45
Shreyas Dethe <shreyasdethe9@gmail.com>
2019-03-12 01:13:35 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,69 @@
+/*
+Creator: Shreyas Dethe
+
+March 12, 2019
+
+Inspired by : The Coding train - https://www.youtube.com/watch?v=Mm2eYfj0SgA
+
+Reference : https://en.wikipedia.org/wiki/Fourier_series
+*/
+
+var n = 0;
+
+var y_ = [];
+
+function setup() {
+	createCanvas(windowWidth, windowHeight);
+}
+
+function draw() {
+	background(0);
+	stroke(255);
+	strokeWeight(2);
+	noFill();
+
+	translate(width/2 - 100, height/2);
+
+	let x = 0;
+	let y = 0;
+
+	for(var i = 0; i < 100; i++){
+
+		let prevx = x;
+		let prevy = y;
+
+		let k = 2*i + 1;	
+		let r = 100 * (4 / (k*PI));
+
+		strokeWeight(4/k);
+		ellipse(x, y, 2*r, 2*r);		//main circle
+
+		x += r*cos(k*n);
+		y += r*sin(k*n);
+
+		strokeWeight(1);
+		ellipse(x, y, 4);				//smaller dot
+
+		strokeWeight(0.25);	
+		line(prevx, prevy, x, y);		//radius line
+
+	}
+
+	strokeWeight(0.5);
+	line(x, y, 250, y);				//guide line
+
+	strokeWeight(1);
+
+	y_.unshift(y);
+
+	noFill();
+	beginShape();
+	for(var i = 0; i < y_.length; i++){
+		vertex(250 + i, y_[i]);
+	}
+	endShape();
+
+	if(y_.length > 250) y_.pop();
+
+	n += 0.01;
+}
